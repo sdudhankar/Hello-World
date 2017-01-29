9fe4f8209071896b42c85b45fe29227d9e0971b7
Sachin Dudhankar <dudhankarsachin16@gmail.com>
2017-01-30 01:42:19 +0530

    Add files via upload

---
+++ app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>Hello {{name}}</h1>',
+})
+export class AppComponent  { name = 'Congrats! your Apps Works fine '; }
+
+
+
